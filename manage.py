f29fa5311dddd04079231aea3b729cea481c67f6
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2010-11-15 20:20:09 +0900

    CSS 置いてみました。

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import sys
 
 from blikit.application import Blikit, is_git_repo
@@ -8,7 +9,10 @@ if not is_git_repo('.'):
     print >> sys.stderr, 'not a git repository'
     sys.exit(0)
 
+static = os.path.join(os.path.dirname(__file__), 'blikit', 'static')
+
 action_runserver = script.make_runserver(lambda : Blikit('.'),
                                          use_reloader=True,
-                                         use_debugger=True)
+                                         use_debugger=True,
+                                         static_files={'/static': static})
 script.run()
