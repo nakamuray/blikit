6b6f6023ec784b7f201fa40b11aae2fd4e47b887
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2010-11-21 23:28:38 +0900

    write setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+import glob
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+setup(
+    name = 'blikit',
+    version = '0.1',
+    packages = ['blikit'],
+    scripts = ['blikit-manage.py'],
+    data_files = [('blikit/static', glob.glob('blikit/static/*')),
+                  ('blikit/templates', glob.glob('blikit/templates/*'))],
+)
+
