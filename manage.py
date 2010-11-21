64eacade482cf958260824bf3aa33cf010da0f92
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2010-11-21 18:29:05 +0900

    python manage.py profile

--- manage.py
+++ manage.py
@@ -4,15 +4,23 @@ import sys
 
 from blikit.application import Blikit, is_git_repo
 from werkzeug import script
+from werkzeug.contrib import profiler
 
 if not is_git_repo('.'):
     print >> sys.stderr, 'not a git repository'
     sys.exit(0)
 
+def make_app():
+    return Blikit('.')
+
 static = os.path.join(os.path.dirname(__file__), 'blikit', 'static')
 
-action_runserver = script.make_runserver(lambda : Blikit('.'),
+action_runserver = script.make_runserver(make_app,
                                          use_reloader=True,
                                          use_debugger=True,
                                          static_files={'/static': static})
+
+action_profile = profiler.make_action(make_app,
+                                      stream=open('/tmp/profiler.log', 'w'))
+
 script.run()
